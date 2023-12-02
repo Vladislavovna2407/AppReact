c5fbc2990c58d588dce497748a5c99da9790ea49
Vladislavovna2407 <Vladislavovna2018@bk.ru>
2023-12-02 15:51:15 +0100

    Hometask4

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,53 +1,48 @@
-import React, { useState } from 'react'
-import { MainButton } from '../../components/mainButton'
-import { Input } from '../../components/Input'
-import { Label } from '../../components/Labels'
-import { Header } from '../../components/Header'
+import React, {useState} from 'react'
+import {MainButton} from '../../components/mainButton'
+import {Input} from '../../components/Input'
+import {Label} from '../../components/Labels'
+import {Header} from '../../components/Header'
 import './styleLogin.css'
 import '../../App.css'
-import { useNavigate } from 'react-router-dom'
-import { LocalizationOptions } from '../PrivateNotes/localizationOptions'
-import { useLocalization } from '../../localization/useLocalization'
+import {useNavigate} from 'react-router-dom'
+import {LocalizationOptions} from '../PrivateNotes/localizationOptions'
+import {useLocalization} from '../../localization/useLocalization'
 
 export const LoginScreen = () => {
-  const { translations } = useLocalization()
+  const {translations} = useLocalization()
   const [name, setName] = useState('')
-  const [surname, setSurname] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
 
   function showUser() {
     let user = {
       username: name,
-      password: password
+      password: password,
     }
-    //console.log(user) //`Username: ${name}, surname: ${surname}, password: ${password}`)
 
-    const url = 'https://dull-pear-haddock-belt.cyclic.app/auth';
+    console.log(user)
+
+    const url = 'https://dull-pear-haddock-belt.cyclic.app/auth'
     fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(user)
+      body: JSON.stringify(user),
     })
-      .then(response =>  response.json()
-     
-      )
+      .then(response => response.json())
       .then(response => {
-        //TODO: save to localstorage
-     
-        localStorage.setItem('token',response.token)
+        localStorage.setItem('token', response.token)
 
-        console.log(response);
+        console.log(response)
 
         setName('')
-        setSurname('')
         setPassword('')
 
         navigate('/private-notes')
       })
-      .catch(error => alert(' User Not Found!'));
+      .catch(() => alert('User Not Found!'))
   }
 
   function changePassword() {
@@ -81,14 +76,3 @@ export const LoginScreen = () => {
     </div>
   )
 }
-
-
-
-/*<Label name={'Surname'} value={translations['surname']} />
-        <Input
-          state={'text'}
-          value1={translations['regester']}
-          answer={'Surname'}
-          value={surname}
-          onChange={event => setSurname(event.target.value)}
-        />*/
